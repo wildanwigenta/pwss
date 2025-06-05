9714ce8b6fee69f08050b35500dbf40e7af24780
Wildan <wildanwigenta52@gmail.com>
2025-06-05 12:53:24 +0700

    add index m8

---
+++ mod8/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD PHP dan MySQLi</title>
+</head>
+<body>
+    <h2>CRUD DATA MAHASISWA</h2>
+    <br />
+    <a href="tambah.php">+ TAMBAH MAHASISWA</a>
+    <br />
+    <br />
+    <table border="1">
+        <tr>
+            <th>NIM</th>
+            <th>NAMA</th>
+            <th>EMAIL</th>
+            <th>NOMOR HP</th>
+            <th>OPSI</th>
+        </tr>
+        <?php
+        include 'koneksi.php';
+        $data = mysqli_query($koneksi, "select * from mahasiswa");
+        while ($d = mysqli_fetch_assoc($data)) {
+        ?>
+            <tr>
+                <td><?php echo $d['nim']; ?></td>
+                <td><?php echo $d['nama']; ?></td>
+                <td><?php echo $d['email']; ?></td>
+                <td><?php echo $d['hp']; ?></td>
+                <td>
+                    <a href="edit.php?nim=<?php echo $d['nim']; ?>">EDIT</a>
+                    <a href="delete.php?nim=<?php echo $d['nim']; ?>">HAPUS</a>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+</body>
+</html>
