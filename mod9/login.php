037dbd29c52e375721b89919627a60732d800232
Wildan <wildanwigenta52@gmail.com>
2025-06-12 09:18:55 +0700

    fix login m9

---
+++ mod9/login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$userName = $_POST['username'];
+$password = $_POST['password'];
+include "koneksi.php";
+$data = $konek->query(
+"SELECT id, username, password, nama FROM admin WHERE username='$userName' 
+AND password='$password'"
+);
+
+//ambil data
+$ketemu = $data->fetch_assoc();
+if ($ketemu) {
+$_SESSION['namaUser']=$ketemu['username'];
+$_SESSION['namaLengkap']=$ketemu ['nama'];
+header ("Location:welcome.php");
+exit();
+}
+else {
+echo "<label>Gak Ketemu </label>";
+echo "<a href='index1.php'>Kembali</a>";
+}
+?>
