8a46038bd5c2357b1ebc108667a16ccccee2111b
Wildan <wildanwigenta52@gmail.com>
2025-06-12 08:44:48 +0700

    add index1 m9

---
+++ mod9/index1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>LOGIN</h1>
+    <form action="login.php" method="post">
+        <label for="un">Username</label><br><br>
+        <input type="text" id="un" name="username" required><br><br>
+
+        <label for="pw">Password</label><br><br>
+        <input type="password" id="pw" name="password" placeholder="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
