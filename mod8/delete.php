3b9805c22fb3996c14e49637ab1346241600115f
Wildan <wildanwigenta52@gmail.com>
2025-06-15 15:00:20 +0700

    clear delete m8

--- mod8/delete.php
+++ mod8/delete.php
@@ -1,10 +1,10 @@
 <?php
-include('koneksi.php');
-$nim=$_GET['nim'];
-$query = "delete from mahasiswa where nim='$nim'";
-$hasil = mysqli_query($koneksi,$query);
-if($hasil)
-header("location:index.php");
- else
- echo' HAPUS Gagal';
+    include('koneksi.php');
+    $nim=$_GET['nim'];
+    $query = "delete from mahasiswa where nim='$nim'";
+    $hasil = mysqli_query($koneksi,$query);
+    if($hasil)
+    header("location:index.php");
+    else
+    echo' HAPUS Gagal';
 ?>
