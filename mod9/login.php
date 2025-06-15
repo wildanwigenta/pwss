1fb0a369b92a42af411a682eae817a64b12e5f40
Wildan <wildanwigenta52@gmail.com>
2025-06-15 14:53:58 +0700

    clear login m9

--- mod9/login.php
+++ mod9/login.php
@@ -4,20 +4,18 @@ $userName = $_POST['username'];
 $password = $_POST['password'];
 include "koneksi.php";
 $data = $konek->query(
-"SELECT id, username, password, nama FROM admin WHERE username='$userName' 
-AND password='$password'"
+    "SELECT id, username, password, nama FROM admin WHERE username='$userName' 
+    AND password='$password'"
 );
 
 //ambil data
 $ketemu = $data->fetch_assoc();
 if ($ketemu) {
-$_SESSION['namaUser']=$ketemu['username'];
-$_SESSION['namaLengkap']=$ketemu ['nama'];
-header ("Location:welcome.php");
-exit();
+    $_SESSION['namaUser'] = $ketemu['username'];
+    $_SESSION['namaLengkap'] = $ketemu['nama'];
+    header("Location:welcome.php");
+    exit();
+} else {
+    echo "<label>Gak Ketemu </label>";
+    echo "<a href='index.php'>Kembali</a>";
 }
-else {
-echo "<label>Gak Ketemu </label>";
-echo "<a href='index.php'>Kembali</a>";
-}
-?>
