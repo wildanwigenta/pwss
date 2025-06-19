e256d59919539e15cb6dfbb45308f19317c8452c
Wildan <wildanwigenta52@gmail.com>
2025-06-19 21:50:03 +0700

    add hapus m10

---
+++ mod10/hapus.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'Database.php';
+include_once 'Pegawai.php';
+$database = new Database();
+$db = $database->getConnection();
+$pegawai = new Pegawai($db);
+if (isset($_GET['id'])) {
+    $pegawai->id_pegawai = $_GET['id'];
+    if ($pegawai->delete()) {
+        echo "<script>alert('Data pegawai berhasil dihapus!');
+window.location.href='index.php';</script>";
+    } else {
+        echo "<script>alert('Gagal menghapus data pegawai.');
+window.location.href='index.php';</script>";
+    }
+} else {
+    die('ERROR: ID tidak ditemukan.');
+}
