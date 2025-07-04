4c5eeba80684ea6aa3fe7d43ad88a0bcf31baa4f
Wildan <wildanwigenta52@gmail.com>
2025-07-04 20:58:53 +0700

    add index hosting

---
+++ hosting/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang</title>
+</head>
+<body>
+    <h1>Halo, ini halaman utama PHP</h1>
+    <p><a href="about.php">Tentang</a> | <a href="contact.php">Kontak</a></p>
+</body>
+</html>
