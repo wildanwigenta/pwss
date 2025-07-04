ab53939e28f34ae37ae1fca4f272df53284c4471
Wildan <wildanwigenta52@gmail.com>
2025-07-04 21:03:43 +0700

    update about

--- hosting/about.php
+++ hosting/about.php
@@ -1,1 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tentang Saya</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Halaman Tentang</h1>
+    <p>Website ini dibuat kelompok G untuk tugas kuliah.</p>
+    <a href="index.php">Beranda</a> | <a href="contact.php">Kontak</a>
+</body>
+</html>
 
