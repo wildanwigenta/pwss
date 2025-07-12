b1c935591d37a7c9896e30e817e1641f37ca8f56
Wildan <wildanwigenta52@gmail.com>
2025-07-12 19:04:55 +0700

    update public index

--- mod11/public/index.php
+++ mod11/public/index.php
@@ -1,8 +1,7 @@
 <?php
 // Definisikan path dasar
 define('BASE_PATH', dirname(__DIR__));
-// Ambil controller dan action dari URL
-// Contoh URL: http://localhost/simpeg_mvc/index.php?controller=pegawai&action=index
+
 $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) . 'Controller' :
     'PegawaiController';
 $actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
