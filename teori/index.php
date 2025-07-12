3ca6367a59c260c9fb867264c50dae4894013858
Wildan <wildanwigenta52@gmail.com>
2025-07-12 19:09:57 +0700

    update index teori

--- teori/index.php
+++ teori/index.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>latihan</title>
 </head>
+
 <body>
-    <?php 
-        echo "<h2>Contoh menghitung Perkalian</h2>";
-        $x =10;
-        $y = 20;
-        $z = 30;
-        $hasil = $x * $y + $z;
-        echo $x." di kali ".$y." ditambah ".$z."<br>";
-        echo "Hasil ".$hasil;
+    <?php
+    echo "<h2>Contoh menghitung Perkalian</h2>";
+    $x = 10;
+    $y = 20;
+    $z = 30;
+    $hasil = $x * $y + $z;
+    echo $x . " di kali " . $y . " ditambah " . $z . "<br>";
+    echo "Hasil " . $hasil;
     ?>
 </body>
+
 </html>
